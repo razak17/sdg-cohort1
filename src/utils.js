1557eb7be8ed6799f9c3ed23910a1e82605af872
AbDuL RaZaK Mo <r17.abdul@gmail.com>
2020-04-11 04:26:37 +0000

    final structure

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+const convertToDays = (data) => {
+  if (data.periodType === 'months') {
+    return data.timeToElapse * 30;
+  }
+  if (data.periodType === 'weeks') {
+    return data.timeToElapse * 7;
+  }
+  return data.timeToElapse;
+};
+
+const requestedTime = (data) => {
+  if (data.periodType === 'months') {
+    return 2 ** Math.trunc((data.timeToElapse * 30) / 3);
+  }
+  if (data.periodType === 'weeks') {
+    return 2 ** Math.trunc((data.timeToElapse * 7) / 3);
+  }
+  return 2 ** Math.trunc(data.timeToElapse / 3);
+};
+
+export { convertToDays, requestedTime };
